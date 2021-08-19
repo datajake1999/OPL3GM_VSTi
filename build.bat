b56233e55b6f0c527bf224812c37a48507590473
datajake1999 <datajake@gmail.com>
2021-08-19 08:44:47 -0700

    Added version information to DLL.

--- build.bat
+++ build.bat
@@ -2,9 +2,11 @@
 call "c:\program files\microsoft visual studio 8\vc\bin\vcvars32.bat"
 call "C:\program files\Microsoft Platform SDK\SetEnv.Cmd" /SRV32 /RETAIL
 cl -c /I ".\msinttypes" /I ".\VST2_SDK" fmopl3lib\*.cpp src\*.cpp synthlib\*.cpp VST2_SDK\public.sdk\source\vst2.x\*.cpp
-link *.obj bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:OPL3GM.dll
+rc src\OPL3GM.rc
+link *.obj src\OPL3GM.res bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:OPL3GM.dll
 del *.obj
 call "C:\program files\Microsoft Platform SDK\SetEnv.Cmd" /X64 /RETAIL
 cl -c /I ".\msinttypes" /I ".\VST2_SDK" fmopl3lib\*.cpp src\*.cpp synthlib\*.cpp VST2_SDK\public.sdk\source\vst2.x\*.cpp
-link *.obj bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:OPL3GM_X64.dll
+link *.obj src\OPL3GM.res bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:OPL3GM_X64.dll
 del *.obj
+del src\OPL3GM.res
