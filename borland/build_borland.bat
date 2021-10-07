07c5917554028bddbf8f3c8920d2fd2b0fb84f44
datajake1999 <datajake@gmail.com>
2021-10-07 07:33:30 -0700

    Updated Borland build script.

--- borland/build_borland.bat
+++ borland/build_borland.bat
@@ -5,14 +5,13 @@ set path=C:\BCC55\Bin;%path%
 set include=C:\BCC55\Include
 set lib=C:\BCC55\Lib
 echo Compiling source files
-bcc32 -c -I..\pstdint;..\VST2_SDK ..\fmopl3lib\*.cpp ..\src\*.cpp ..\synthlib_apogee\*.cpp ..\synthlib_doom\*.cpp ..\synthlib_w9x\*.cpp ..\VST2_SDK\public.sdk\source\vst2.x\*.cpp
+bcc32 -q -c -I..\pstdint;..\VST2_SDK -w-8004 -w-8057 ..\fmopl3lib\*.cpp ..\src\*.cpp ..\synthlib_apogee\*.cpp ..\synthlib_doom\*.cpp ..\synthlib_w9x\*.cpp ..\VST2_SDK\public.sdk\source\vst2.x\*.cpp
 echo Compiling resource script
-brcc32 ..\src\OPL3GM.rc
-move ..\src\OPL3GM.RES
+brcc32 -fo OPL3GM.res ..\src\OPL3GM.rc
 echo Linking DLLs
-ilink32 /Tpd c0d32.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj al_midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Apogee.dll,,cw32mt.lib import32.lib,vstplug.def,OPL3GM.RES
-ilink32 /Tpd c0d32.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj i_oplmusic.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Doom.dll,,cw32mt.lib import32.lib,vstplug.def,OPL3GM.RES
-ilink32 /Tpd c0d32.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj opl3midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3W9x.dll,,cw32mt.lib import32.lib,vstplug.def,OPL3GM.RES
+ilink32 -q /Tpd c0d32.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj al_midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Apogee.dll,,cw32mt.lib import32.lib,vstplug.def,OPL3GM.res
+ilink32 -q /Tpd c0d32.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj i_oplmusic.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Doom.dll,,cw32mt.lib import32.lib,vstplug.def,OPL3GM.res
+ilink32 -q /Tpd c0d32.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj opl3midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3W9x.dll,,cw32mt.lib import32.lib,vstplug.def,OPL3GM.res
 echo Cleaning up intermediate build files
 del *.ilc *.ild *.ilf *.ils *.map *.obj *.res *.tds
 echo Build finished
