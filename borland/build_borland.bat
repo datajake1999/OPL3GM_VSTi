9e6703474c9bb57e9a0c57343760213aba77367d
datajake1999 <datajake@gmail.com>
2021-12-10 08:01:04 -0800

    Moved resource script to res directory.

--- borland/build_borland.bat
+++ borland/build_borland.bat
@@ -7,7 +7,7 @@ set lib=C:\BCC55\Lib
 echo Compiling source files.
 bcc32 -q -c -I..\pstdint;..\VST2_SDK -w-8004 -w-8057 ..\fmopl3lib\*.cpp ..\src\*.cpp ..\synthlib_apogee\*.cpp ..\synthlib_doom\*.cpp ..\synthlib_w9x\*.cpp ..\VST2_SDK\public.sdk\source\vst2.x\*.cpp
 echo Compiling resource script.
-brcc32 -fo OPL3GM.res ..\src\OPL3GM.rc
+brcc32 -fo OPL3GM.res ..\res\OPL3GM.rc
 echo Linking DLLs.
 ilink32 -q /Tpd c0d32.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj resampler.obj al_midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Apogee.dll,,cw32mt.lib import32.lib,vstplug.def,OPL3GM.res
 ilink32 -q /Tpd c0d32.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj resampler.obj i_oplmusic.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Doom.dll,,cw32mt.lib import32.lib,vstplug.def,OPL3GM.res
