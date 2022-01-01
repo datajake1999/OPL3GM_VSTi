23ba04ca181790da2f3e0ca34b7ea1637a8a72c5
datajake1999 <datajake@gmail.com>
2021-12-31 23:41:18 -0800

    Added icon alt text as string table resource.

--- res/resource.h
+++ res/resource.h
@@ -3,12 +3,13 @@
 // Used by OPL3GM.rc
 //
 #define IDI_ICON1                       101
+#define ICON_ALTTEXT1                       201
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        202
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
