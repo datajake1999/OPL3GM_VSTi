b12888601a7ae8cdb914bd9052e691346636e391
datajake1999 <datajake@gmail.com>
2022-04-28 18:38:16 -0700

    Moved reaper after queue In link commands.

--- scripts/build_dmc.bat
+++ scripts/build_dmc.bat
@@ -9,9 +9,9 @@ dmc -c -DVST_FORCE_DEPRECATED=0 -I..\VST2_SDK @src.lst
 echo Compiling resource script.
 rcc -32 ..\res\OPL3GM.rc
 echo Linking DLLs.
-link /exetype:nt DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj reaper.obj queue.obj al_midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Apogee.dll,,kernel32.lib user32.lib,..\res\vstplug.def,OPL3GM.res
-link /exetype:nt DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj reaper.obj queue.obj i_oplmusic.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Doom.dll,,kernel32.lib user32.lib,..\res\vstplug.def,OPL3GM.res
-link /exetype:nt DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj reaper.obj queue.obj opl3midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3W9x.dll,,kernel32.lib user32.lib,..\res\vstplug.def,OPL3GM.res
+link /exetype:nt DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj queue.obj reaper.obj al_midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Apogee.dll,,kernel32.lib user32.lib,..\res\vstplug.def,OPL3GM.res
+link /exetype:nt DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj queue.obj reaper.obj i_oplmusic.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Doom.dll,,kernel32.lib user32.lib,..\res\vstplug.def,OPL3GM.res
+link /exetype:nt DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj queue.obj reaper.obj opl3midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3W9x.dll,,kernel32.lib user32.lib,..\res\vstplug.def,OPL3GM.res
 echo Cleaning up intermediate build files.
 del *.map *.obj *.res
 echo Build finished. Press any key to continue.
