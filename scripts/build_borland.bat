7443198fdd03448cd8f943ff7dca4b5408706293
datajake1999 <datajake@gmail.com>
2022-06-05 18:29:33 -0700

    Updated build scripts.

--- scripts/build_borland.bat
+++ scripts/build_borland.bat
@@ -5,13 +5,13 @@ set path=C:\BCC55\Bin;%path%
 set include=C:\BCC55\Include
 set lib=C:\BCC55\Lib
 echo Compiling source files.
-bcc32 -q -c -DVST_FORCE_DEPRECATED=0 -I..\include\pstdint;..\VST2_SDK -w-8004 -w-8057 @src.lst
+bcc32 -q -c -DVST_FORCE_DEPRECATED=0 -Dgui -I..\include\pstdint;..\VST2_SDK -w-8004 -w-8057 @src.lst
 echo Compiling resource script.
 brcc32 -fo OPL3GM.res ..\res\OPL3GM.rc
 echo Linking DLLs.
-ilink32 -q /Tpd c0d32.obj DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj queue.obj reaper.obj al_midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Apogee.dll,,cw32mt.lib import32.lib,..\res\borland.def,OPL3GM.res
-ilink32 -q /Tpd c0d32.obj DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj queue.obj reaper.obj i_oplmusic.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Doom.dll,,cw32mt.lib import32.lib,..\res\borland.def,OPL3GM.res
-ilink32 -q /Tpd c0d32.obj DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj queue.obj reaper.obj opl3midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3W9x.dll,,cw32mt.lib import32.lib,..\res\borland.def,OPL3GM.res
+ilink32 -q /Tpd c0d32.obj DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj queue.obj reaper.obj editor.obj al_midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Apogee.dll,,cw32mt.lib import32.lib,..\res\borland.def,OPL3GM.res
+ilink32 -q /Tpd c0d32.obj DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj queue.obj reaper.obj editor.obj i_oplmusic.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3Doom.dll,,cw32mt.lib import32.lib,..\res\borland.def,OPL3GM.res
+ilink32 -q /Tpd c0d32.obj DCFilter.obj resampler.obj opl.obj opl3.obj opl3class.obj main.obj midiprog.obj OPL3GM.obj OPL3GM_proc.obj queue.obj reaper.obj editor.obj opl3midi.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,OPL3W9x.dll,,cw32mt.lib import32.lib,..\res\borland.def,OPL3GM.res
 echo Cleaning up intermediate build files.
 del *.ilc *.ild *.ilf *.ils *.map *.obj *.res *.tds
 echo Build finished. Press any key to continue.
