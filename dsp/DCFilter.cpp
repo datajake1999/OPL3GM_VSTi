f56a6fb8c8765f663aa69571f75b9c3fdbc3866e
datajake1999 <datajake@gmail.com>
2022-06-10 15:50:06 -0700

    DC blocking filter also serves as noise gate.

--- dsp/DCFilter.cpp
+++ dsp/DCFilter.cpp
@@ -1,4 +1,5 @@
 #include "DCFilter.h"
+#include <math.h>
 
 DCFilter::DCFilter()
 {
@@ -9,6 +10,10 @@ DCFilter::DCFilter()
 double DCFilter::Process(double in)
 {
 	double out = (in - LastIn) + (0.999 * LastOut);
+	if (fabs(out) < 0.0001)
+	{
+		out = 0;
+	}
 	LastIn = in;
 	LastOut = out;
 	return out;
