7eca3fe9ea9a404b20016b6afe108b36b98021b7
datajake1999 <datajake@gmail.com>
2022-06-30 11:06:15 -0700

    Fixed spacing of definitions In resource.h.

--- res/resource.h
+++ res/resource.h
@@ -3,50 +3,50 @@
 // Used by OPL3GM.rc
 //
 #define IDI_ICON1                       101
-#define IDS_ICON_ALTTEXT1                       201
-#define IDS_ABOUTCAP                       202
-#define IDS_ABOUTTXT                       203
-#define IDS_STATSCAP                       204
-#define IDS_STATSTXT                       205
-#define IDS_HOSTCAP                       206
-#define IDS_HOSTTXT                       207
-#define IDS_FILEFLT                       208
-#define IDS_FILECAP                       209
-#define IDS_W9XCAP                       210
-#define IDS_W9XTXT                       211
-#define IDS_FAILCAP                       212
-#define IDS_FAILTXT                       213
-#define IDS_HELPCAP                       214
-#define IDS_HELPTXT                       215
-#define IDS_WEBCAP                       216
-#define IDS_WEBTXT                       217
-#define IDS_REAPPROVE                       218
-#define IDD_DIALOG                       301
+#define IDS_ICON_ALTTEXT1               201
+#define IDS_ABOUTCAP                    202
+#define IDS_ABOUTTXT                    203
+#define IDS_STATSCAP                    204
+#define IDS_STATSTXT                    205
+#define IDS_HOSTCAP                     206
+#define IDS_HOSTTXT                     207
+#define IDS_FILEFLT                     208
+#define IDS_FILECAP                     209
+#define IDS_W9XCAP                      210
+#define IDS_W9XTXT                      211
+#define IDS_FAILCAP                     212
+#define IDS_FAILTXT                     213
+#define IDS_HELPCAP                     214
+#define IDS_HELPTXT                     215
+#define IDS_WEBCAP                      216
+#define IDS_WEBTXT                      217
+#define IDS_REAPPROVE                   218
+#define IDD_DIALOG                      301
 #ifndef IDC_STATIC
-#define IDC_STATIC                       -1
+#define IDC_STATIC                      -1
 #endif
-#define IDC_PRESET                       401
-#define IDC_PRESETNAME                       402
-#define IDC_VOLUME                       403
-#define IDC_VOLDISP1                       404
-#define IDC_VOLDISP2                       405
-#define IDC_TRANSPOSE                       406
-#define IDC_TRANDISP1                       407
-#define IDC_TRANDISP2                       408
-#define IDC_DISPLAY                       409
+#define IDC_PRESET                      401
+#define IDC_PRESETNAME                  402
+#define IDC_VOLUME                      403
+#define IDC_VOLDISP1                    404
+#define IDC_VOLDISP2                    405
+#define IDC_TRANSPOSE                   406
+#define IDC_TRANDISP1                   407
+#define IDC_TRANDISP2                   408
+#define IDC_DISPLAY                     409
 #define IDC_NUKED                       410
-#define IDC_DC                       411
+#define IDC_DC                          411
 #define IDC_QUEUE                       412
 #define IDC_PANIC                       413
 #define IDC_RESET                       414
-#define IDC_VOICECOUNT                       415
-#define IDC_REFRESH                       416
-#define IDC_LOAD                       417
-#define IDC_CURBANK                       418
+#define IDC_VOICECOUNT                  415
+#define IDC_REFRESH                     416
+#define IDC_LOAD                        417
+#define IDC_CURBANK                     418
 #define IDC_ABOUT                       419
 #define IDC_STATS                       420
-#define IDC_HOSTINFO                       421
-#define IDC_PROJPAGE                       422
+#define IDC_HOSTINFO                    421
+#define IDC_PROJPAGE                    422
 
 // Next default values for new objects
 // 
