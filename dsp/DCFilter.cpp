939be6ebd77601fce2edf480a47622830379b014
datajake1999 <datajake@gmail.com>
2022-07-11 16:26:26 -0700

    Improved noise gate.

--- dsp/DCFilter.cpp
+++ dsp/DCFilter.cpp
@@ -9,23 +9,51 @@ DCFilter::DCFilter()
 	pole = 1;
 	LastIn = 0;
 	LastOut = 0;
+	open = false;
+	envelope = 0;
+	threshold = 0;
+	FadeIn = 0;
+	FadeOut = 0;
+	HoldLength = 0;
+	HoldTime = 0;
 }
 
 void DCFilter::SetRate(double rate)
 {
-	double wn = M_PI * (10.0 / rate);
+	double wn = M_PI * (fltfreq / rate);
 	double b0 = 1.0 / (1.0 + wn);
 	pole = (1.0 - wn) * b0;
+	threshold = pow(10.0, thresh / 20.0);
+	FadeIn = 1.0 / pow(10.0, 1.0 / (rate * fadeinlen / 1000.0));
+	FadeOut = 1.0 / pow(10.0, 1.0 / (rate * fadeoutlen / 1000.0));
+	HoldLength = rate * holdlen / 1000.0;
 }
 
 double DCFilter::Process(double in)
 {
 	double out = (in - LastIn) + (pole * LastOut);
-	if (fabs(out) < 0.0001)
-	{
-		out = 0;
-	}
 	LastIn = in;
 	LastOut = out;
+	if (fabs(out) > threshold)
+	{
+		open = true;
+		HoldTime = 0;
+	}
+	else
+	{
+		if (HoldTime++ > HoldLength)
+		{
+			open = false;
+		}
+	}
+	if (open)
+	{
+		envelope = (envelope * FadeIn) + (1.0 - FadeIn);
+	}
+	else
+	{
+		envelope = envelope * FadeOut;
+	}
+	out = out * envelope;
 	return out;
 }
--- dsp/DCFilter.h
+++ dsp/DCFilter.h
@@ -1,6 +1,12 @@
 #ifndef DCFILTER_H
 #define DCFILTER_H
 
+#define fltfreq 10.0
+#define thresh -66.0
+#define fadeinlen 1.0
+#define fadeoutlen 100.0
+#define holdlen 10.0
+
 class DCFilter {
 public:
 	DCFilter();
@@ -10,6 +16,13 @@ private:
 	double pole;
 	double LastIn;
 	double LastOut;
+	bool open;
+	double envelope;
+	double threshold;
+	double FadeIn;
+	double FadeOut;
+	double HoldLength;
+	double HoldTime;
 };
 
 #endif
